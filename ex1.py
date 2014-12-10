d14b7eb0acdef98978e2c380711da7836c06d64a
Sara Lindström <saralindstrom@dhcp-10-181-19-181.dynamic.eduroam.mwn.de>
2014-12-10 21:03:43 +0100

    My first commit

---
+++ ex1.py
@@ -0,0 +1,18 @@
+#my first program
+
+print "hello world" 
+print "hello again"
+print "I like typing this"
+print "this is fun."
+print 'Yay! Printing'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# A comment, this is so you can read your python later.
+# Anything after the # is ignored by python.
+
+print 'I could have code like this.' # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print 'this won't run.'
+print 'This will run.'
